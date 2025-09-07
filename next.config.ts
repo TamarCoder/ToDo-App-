c3e91da8262ca2a9acd7b89663ff51adf690cee8
Tamar khuskivadze <thamuna.khuskivadze@gmail.com>
2025-09-07 16:52:07 +0400

    feat : initial aplication

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+    images: {
+        domains: ["images.unsplash.com"],
+        unoptimized: true,
+    },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
